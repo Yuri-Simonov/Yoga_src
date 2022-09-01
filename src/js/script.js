5d109531d83c7ed6dee7919e1ee49faca5e47b75
Yuri Simonov <y.simonov@alef.im>
2022-09-01 09:04:10 +0300

    добавлена логика для меню + мелкие правки

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,21 @@
 // Импорт функции проверки поддержки браузера формата WEBP для картинок
 import "./modules/support_Webp.js";
+//=====================================================================
+
+const burger = document.querySelector(".header__burger");
+const menu = document.querySelector(".menu");
+const menuOverlay = document.querySelector(".menu__overlay");
+// Открытие и закрытие бокового меню на ширине <1500px
+const toggleBurger = () => {
+	if (menu.classList.contains("menu_active")) {
+		menu.classList.remove("menu_active");
+	} else {
+		menu.classList.add("menu_active");
+	}
+};
+burger.addEventListener("click", (event) => {
+	toggleBurger();
+});
+menuOverlay.addEventListener("click", (event) => {
+	toggleBurger();
+});
